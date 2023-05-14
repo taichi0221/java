0d98cb2b4bb2c35be843ea8235f321243ef67eb2
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-05-14 13:02:10 +0900

    比較演算子

--- myjava/Greeting.java
+++ myjava/Greeting.java
@@ -1,20 +1,12 @@
 class Greeting {
   public static void main(String args[]){
-    float var
-    byte var01 = 1;
-    short var02 = 12345;
-    int var03 = 1234567890;
-    long var04 = 12345678900L;
-    System.out.println(var01);
-    System.out.println(var02);
-    System.out.println(var03);
-    System.out.println(var04);
+    int x = 10;
+    int y = 2;
 
-    int var;
-    var = 1;
-    System.out.println(var);
-    System.out.println("Good morning");
-  
-    System.out.println("こんにちは");
+    System.out.println(x > y);
+    System.out.println(x < y);
+    System.out.println(x * y);
+    System.out.println(x / y);
+    System.out.println(x % y);
   }
 }
