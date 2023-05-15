664d7c4d45554f3e9ce7dda57f829153bf4d06d7
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-05-15 22:33:30 +0900

    Greeting

--- myjava/Greeting.java
+++ myjava/Greeting.java
@@ -1,10 +1,10 @@
 class Greeting {
   public static void main(String args[]){
-    int x = 10;
-    int y = 2;
+    int x = 8;
+    int y = 3;
 
-    System.out.println(x > y);
-    System.out.println(x < y);
+    System.out.println(x == y);
+    System.out.println(x != y);
     System.out.println(x * y);
     System.out.println(x / y);
     System.out.println(x % y);
