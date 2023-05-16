8e2d9dbd6bd815663a6e455413036cc2e04c3c28
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-05-16 13:04:11 +0900

    for 文

--- myjava/Greeting.java
+++ myjava/Greeting.java
@@ -2,14 +2,10 @@ import java.util.ArrayList;
 
 class Greeting {
   public static void main(String args[]){
-    int value = 3;
+    int[] scores = {1 ,5 ,10};
 
-    if (value > 0){
-      System.out.println("値は正です");
-    }else if (value < 0){
-      System.out.println("値は負です");
-    }else {
-      System.out.println("値は０です");
+    for(int score : scores) {
+      System.out.println(score);
     }
   }
 }
