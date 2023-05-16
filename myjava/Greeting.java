6001b057b94bc924de147abf3db9fe1f0bc7840a
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-05-16 11:06:30 +0900

    if文

--- myjava/Greeting.java
+++ myjava/Greeting.java
@@ -2,16 +2,14 @@ import java.util.ArrayList;
 
 class Greeting {
   public static void main(String args[]){
-  ArrayList<Integer> scores = new ArrayList<Integer>();
+    int value = 3;
 
-  scores.add(1);
-  scores.add(5);
-  scores.add(10);
-  scores.add(15);
-
-  System.out.println(scores.get(0));
-  System.out.println(scores.get(1));
-  System.out.println(scores.get(2));
-  System.out.println(scores.get(3));
+    if (value > 0){
+      System.out.println("値は正です");
+    }else if (value < 0){
+      System.out.println("値は負です");
+    }else {
+      System.out.println("値は０です");
+    }
   }
 }
